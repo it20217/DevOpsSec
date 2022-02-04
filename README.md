d3c7be2a18af48f55726eccce23269e2dac98af2
Igor Tokarev <igor.tokarev@outlook.com>
2022-02-04 19:48:56 +0100

    Mistakes in script removed, readme file updated

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Week 2 Lab – Bash & Git
 Igor Tokarev, student number 21138575
 [Link for Week 2 Lab – Bash & Git](https://github.com/it20217/DevOpsSec)
+---
+To run the script.sh run: `bash script.sh`
--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-
+rmdir fol_1
+rmdir fol_2
 mkdir fol_1
 mkdir fol_2
 cd fol_1
@@ -11,8 +12,4 @@ touch 2_1.txt 2_2.txt 2_3.txt
 chmod 700 *_1.txt *_3.txt
 chmod 777 *_2.txt
 cd ../
-remdir fol_1
-remdir fol_2
----
-To run the script.sh run: `bash script.sh`
 echo "job completed"
